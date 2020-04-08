e4571fb25d34107307f7e25a3be86ee09c1d5821
Reynold Tan <reynoldltan@gmail.com>
2020-04-08 12:49:31 -0600

    DB and Search Call

---
+++ includes/forms/tripal_ws_script_form.js
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * Contains scripts to manage behaviours used in configuration page.
+ */
+
+(function($) {
+  Drupal.behaviors.tripalWSConfigure = {
+    attach: function (context, settings) {
+      //
+      // Set this field as to the default value selected.
+      $('.tripal-ws-field-select-default select').change(function() {
+        var defValue = $(this).val();
+
+        if (defValue) {
+          $('#tripal-ws-config-version-default-value')
+            .val(defValue);
+        }
+      });
+
+      // Confirm action.
+      $('.tripal-ws-rem').click(function(i) {
+        var r = confirm('Are you sure want to remove this item?');
+        if (!r) return false;
+      });
+      //
+} }; }(jQuery));
