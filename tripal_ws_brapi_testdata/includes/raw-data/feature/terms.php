0f2a1e0c10f4aff35266f6417f400b982f3b2a2b
Reynold Tan <reynoldltan@gmail.com>
2020-08-26 19:18:50 -0600

    Additional csv
    - Created additional csv file to allow test to call /markers

--- tripal_ws_brapi_testdata/includes/raw-data/feature/terms.php
+++ tripal_ws_brapi_testdata/includes/raw-data/feature/terms.php
@@ -4,5 +4,6 @@
 // CV = [term1, term2, ...]
 $terms = [
   'MAIN' => ['marker_locus'],
-  'sequence' => ['linkage_group']
+  'sequence' => ['linkage_group', 'genetic_marker'],
+  'relationship' => ['instance_of']
 ];
